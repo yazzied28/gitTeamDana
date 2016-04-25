a8f73b6fcc29336d8f2eba4b756b6e18fb635a62
Ryan Sam <rnsam@asu.edu>
2016-04-25 15:49:18 -0600

    added h2 headers

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>team</title>
   </head>
   <body>
     <h1>This is me Dana's project. </h1>
+    <h2>Ryan has added to the project</h2>
+
+    <h1>This is me Dana's project. </h1>
+    <h2>Ryan has added to the project</h2>
   </body>
 </html>
